57a3cf5d391a73397330fbfc27c67f652f3c8751
ivanbrennan <ivan.brennan@gmail.com>
2013-11-22 18:08:27 -0500

    ssh key add takes 2 parameters

--- lib/digitalocean/ssh_key.rb
+++ lib/digitalocean/ssh_key.rb
@@ -18,8 +18,8 @@ module Digitalocean
       RecursiveOpenStruct.new(response.body, :recurse_over_arrays => true)
     end
 
-    def self.add(attrs)
-      response = Digitalocean.request.get "ssh_keys/new/?name=#{attrs[:ssh_key_name]}&ssh_pub_key=#{attrs[:ssh_public_key]}"
+    def self.add(ssh_key_name, ssh_public_key)
+      response = Digitalocean.request.get "ssh_keys/new/?name=#{ssh_key_name}&ssh_pub_key=#{ssh_public_key}"
       RecursiveOpenStruct.new(response.body, :recurse_over_arrays => true)
     end
   end
