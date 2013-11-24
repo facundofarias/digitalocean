77af55d2ea69f16fc1c85411e73e40218ae121d4
ivanbrennan <ivan.brennan@gmail.com>
2013-11-23 20:21:32 -0500

    ssh-key edit method

--- lib/digitalocean/ssh_key.rb
+++ lib/digitalocean/ssh_key.rb
@@ -22,5 +22,10 @@ module Digitalocean
       response = Digitalocean.request.get "ssh_keys/new/?name=#{ssh_key_name}&ssh_pub_key=#{ssh_public_key}"
       RecursiveOpenStruct.new(response.body, :recurse_over_arrays => true)
     end
+
+    def self.edit(ssh_key_id=nil)
+      response = Digitalocean.request.get "ssh_keys/#{ssh_key_id}/edit"
+      RecursiveOpenStruct.new(response.body, :recurse_over_arrays => true)
+    end
   end
 end
